4a20e4170d82d200277e07541abd5a1648ce6aa8
Francine Dalida <francine.dalida@gmail.com>
2025-07-04 23:43:21 +0800

    fixed: cards

--- web-portfolio/src/components/ProjectCard.jsx
+++ web-portfolio/src/components/ProjectCard.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 
 const ProjectCard = ({ image, alt, title, description }) => {
-  <div className="card h-100 bg-[#181A20] rounded-2xl shadow-lg overflow-hidden">
-    <img
-      src={image}
-      className="card-img-top object-cover h-48 w-full"
-      alt={alt}
-    />
-    <div className="card-body p-6">
-      <h5 className="card-title text-2xl font-bold text-[#4880C9] mb-2">
-        {title}
-      </h5>
-      <p className="card-text text-[#D4D4D4]">{description}</p>
-    </div>
-  </div>;
+  return (
+    <>
+      <div className="card h-100 bg-[#181A20] rounded-2xl shadow-lg overflow-hidden transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_30px_5px_#4880C9]">
+        <img
+          src={image}
+          className="card-img-top object-cover h-48 w-full"
+          alt={alt}
+        />
+        <div className="card-body p-6">
+          <h5 className="card-title text-2xl font-bold text-[#4880C9] mb-2">
+            {title}
+          </h5>
+          <p className="card-text text-[#D4D4D4]">{description}</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProjectCard;
